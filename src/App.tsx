ea7186b74fdf36e86277a18b2b52819adf50212b
BsoftLimited <bsoftlimited@gmail.com>
2023-10-11 08:06:39 +0100

    finished with the tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import './index.css';
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 20;
 
 const App = () => {
   const searchInput = useRef<HTMLInputElement| null>(null);
-  const [query, setQuery] = useState<string>();
+  const [images, setImages] = useState<any[]>([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const fetchImages = useCallback(async () => {
+      try {
+          if (searchInput.current?.value) {
+              setErrorMsg('');
+              setLoading(true);
+              const { data } = await axios.get(`${API_URL}?query=${searchInput.current?.value}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`);
+              console.log('data', data);
+              setImages(data.results);
+              setTotalPages(data.total_pages);
+              setLoading(false);
+          }
+      } catch (error) {
+          setErrorMsg('Error fetching images. Try again later.');
+          console.log(error);
+          setLoading(false);
+      }
+  }, [page]);
+
+  useEffect(() => {fetchImages(); }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
+  
 
   const handleSearch = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('submitted');
+    console.log(searchInput.current?.value);
+    resetSearch();
   };
 
   const handleSelection = (selection: string) => {
-    setQuery(selection);
+    if(searchInput.current){
+      searchInput.current.value = selection;
+      resetSearch();
+    }
   }
 
   return (
     <div className='container'>
       <h1 className='title'>Image Search</h1>
+      {errorMsg && <p className='error-msg'>{errorMsg}</p>}
       <div className='search-section'>
         <Form onSubmit={handleSearch}>
-          <Form.Control type='search' placeholder='Type something to search...' className='search-input' value={query} ref={searchInput} />
+          <Form.Control type='search' placeholder='Type something to search...' className='search-input' ref={searchInput} />
         </Form>
       </div>
       <div className='filters'>
@@ -29,6 +68,16 @@ const App = () => {
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
+      { !loading && (<>
+        <div className='images'>
+          {images.map((image) => (<img key={image.id} src={image.urls.small} alt={image.alt_description} className='image' />) )}
+        </div>
+        <div className='buttons'>
+          { page > 1 && (<Button onClick={() => setPage(page - 1)}>Previous</Button> )}
+          { page < totalPages && (<Button onClick={() => setPage(page + 1)}>Next</Button>)}
+        </div>
+      </>) }
+      { loading && <p className='loading'>Loading...</p> }
     </div>
   );
 };
